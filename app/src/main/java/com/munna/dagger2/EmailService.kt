f8dbf1522abad7ea60d06f23f59d09d36f53f8c4
Munna Gupta <munna.g@peopleinteractive.in>
2022-07-24 20:00:19 +0530

    Singleton at component level

--- app/src/main/java/com/munna/dagger2/EmailService.kt
+++ app/src/main/java/com/munna/dagger2/EmailService.kt
@@ -2,7 +2,9 @@ package com.munna.dagger2
 
 import android.util.Log
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class EmailService @Inject constructor () {
 
     fun sent(){
--- app/src/main/java/com/munna/dagger2/di/UserRegistrationComponent.kt
+++ app/src/main/java/com/munna/dagger2/di/UserRegistrationComponent.kt
@@ -3,7 +3,9 @@ package com.munna.dagger2.di
 import com.munna.dagger2.MainActivity
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [NotificationServiceModule::class, UserRepoServiceModule::class])
 interface UserRegistrationComponent {
 
